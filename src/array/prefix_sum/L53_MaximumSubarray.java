3c9318061bb2af108d099770d90571e56cf0d9fb
TaiVT <>
2022-06-27 00:01:28 +0700

    53. Maximum Subarray

---
+++ src/array/prefix_sum/L53_MaximumSubarray.java
@@ -0,0 +1,36 @@
+package array.prefix_sum;
+
+/**
+ * @author taivt
+ * @link https://leetcode.com/problems/maximum-subarray/
+ * @since 2022/06/26 21:25:22
+ */
+public class L53_MaximumSubarray {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(solution.maxSubArray(new int[]{1}));
+        System.out.println(solution.maxSubArray(new int[]{5, 4, -1, 7, 8}));
+    }
+
+    // Space Complexity: O(n)
+    // Time Complexity: O(n)
+    private static class Solution {
+        public int maxSubArray(int[] nums) {
+            int[] prefixSum = new int[nums.length];
+            prefixSum[0] = nums[0];
+            for (int i = 1; i < prefixSum.length; i++) {
+                prefixSum[i] = prefixSum[i - 1] + nums[i];
+            }
+
+            int maxSum = Integer.MIN_VALUE;
+            int minSoFar = 0;
+            for (int i = 0; i < prefixSum.length; i++) {
+                maxSum = Math.max(maxSum, prefixSum[i] - minSoFar);
+                minSoFar = Math.min(minSoFar, prefixSum[i]);
+            }
+            return maxSum;
+        }
+    }
+}
